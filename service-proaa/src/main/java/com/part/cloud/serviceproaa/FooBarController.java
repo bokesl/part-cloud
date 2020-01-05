fb9129b67fc481d4522f6a90b8fc004650a419ee
leslia <959319414@qq.com>
2020-01-05 18:55:41 +0800

    jenkins service proaa

--- service-proaa/src/main/java/com/part/cloud/serviceproaa/FooBarController.java
+++ service-proaa/src/main/java/com/part/cloud/serviceproaa/FooBarController.java
@@ -1,12 +1,12 @@
 package com.part.cloud.serviceproaa;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RefreshScope
+//@RestController
+//@RefreshScope
 public class FooBarController {
 
     @Value("${foo.bar}")
