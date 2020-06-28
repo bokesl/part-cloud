41594eba9e88d3c90b3296e6e1570e0717aae512
leslia <959319414@qq.com>
2020-06-29 03:21:49 +0800

    update HiController

--- service-conaa/src/main/java/com/part/cloud/serviceconaa/HiController.java
+++ service-conaa/src/main/java/com/part/cloud/serviceconaa/HiController.java
@@ -1,7 +1,6 @@
 package com.part.cloud.serviceconaa;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
